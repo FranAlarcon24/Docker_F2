82f5d2a4332015be1c7f02d54d549f0f98943679
Francisca Antonia Alarcón Saldaña <franc.alarcons@duocuc.cl>
2025-10-30 14:43:07 -0300

    sin la carpeta de la carpeta

---
+++ empezando-docker-1/src/test/java/com/example/AppTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+
+    @Test
+    public void testMain() {
+        // Aquí puedes agregar pruebas para verificar el comportamiento de la clase App
+        assertEquals(1, 1); // Ejemplo de prueba simple
+    }
+}
